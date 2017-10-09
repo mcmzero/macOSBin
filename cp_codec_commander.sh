59a90f90ff15c2f8ef75d49b698b5a1a601847fd
changmin <changmin811@gmail.com>
2017-10-09 13:43:38 +0900

    update

--- cp_codec_commander.sh
+++ cp_codec_commander.sh
@@ -5,4 +5,4 @@ sudo cp -avf /Volumes/ESP/EFI/CLOVER/kexts/Other/CodecCommander.kext /System/Lib
 sudo chmod -R 755 /System/Library/Extensions/CodecCommander.kext
 sudo chown -R root:wheel /System/Library/Extensions/CodecCommander.kext
 
-kextcache -i /
+sudo kextcache -i /
