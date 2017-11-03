965e868f331493acb7024176baa60530a94ad8a8
changmin <changmin811@gmail.com>
2017-11-03 11:35:04 +0900

    add downloading code

--- install_nv_webdriver_v3.sh
+++ install_nv_webdriver_v3.sh
@@ -1,52 +1,64 @@
 #!/bin/sh
 
-FAKEVERSION=$1
-if [ "${FAKEVERSION}" == "" ];
-then
-        FAKEVERSION=17B48
-fi
+PKG_FILE_VERSION=378.10.10.10.20.107
+PKG_OSVERSION=17B48
 
-#OSVERSION=$(sysctl kern.osversion | awk '{ print $2 }')
+if [ "$1" != "" ]; then PKG_OSVERSION=$1; fi
+PKG_FILE=WebDriver-${PKG_FILE_VERSION}.pkg
+PKG_MAJOR_VERSION=$(echo $PKG_FILE_VERSION | cut -d . -f 1)
+PKG_URL=https://images.nvidia.com/mac/pkg/${PKG_MAJOR_VERSION}/${PKG_FILE}
+
+SYSTEM_VERSION_FILE=/System/Library/CoreServices/SystemVersion.plist
 OSVERSION=$(sw_vers -buildVersion)
-
-echo "잠시 시스템 빌드번호 ${OSVERSION}를${FAKEVERSION}로 변경합니다"
-sudo sed -e "s/$OSVERSION/$FAKEVERSION/" -i '' /System/Library/CoreServices/SystemVersion.plist
-SYSTEM_VERSION=$(grep $FAKEVERSION /System/Library/CoreServices/SystemVersion.plist | cut -d "<" -f 2 | cut -d ">" -f 2)
-echo "시스템 버전 확인: $SYSTEM_VERSION"
-echo
-
-WEBDRIVER_FILE=$(ls -tr WebDriver-*.pkg | head -n 1)
-WEBDRIVER_PKG=$(echo $WEBDRIVER_FILE | cut -d "." -f 7)
-if [ "$WEBDRIVER_PKG" == "pkg" ]; then
-        echo "패키지 파일 ${WEBDRIVER_FILE}를 찾았습니다."
-        echo "다음 명령으로 설치를 진행합니다."
-        echo "sudo installer -pkg $WEBDRIVER_FILE -target /"
-        echo
-        sudo installer -pkg $WEBDRIVER_FILE -target /
-else
-        echo "웹드라이버 패키지를 실행 하세요."
-        sleep 3
-        echo
-
-        echo "웹드라이버 설치에서 \"재시작\" 버튼이 나오면 여기에 \"엔터\"를 치세요."
-        read
+SYSCTL_OSVERSION=$(sysctl kern.osversion | cut -d ' ' -f2)
+if [ "${OSVERSION}" == "" ]; then
+        sudo sed -e "s/\<string\>$OSVERSION\<\/string\>/\<string\>$SYSCTL_OSVERSION\<\/string\>/" -i '' ${SYSTEM_VERSION_FILE}
+        OSVERSION=$(sw_vers -buildVersion)
 fi
 
-echo "다시 시스템 빌드번호 ${FAKEVERSION}에서 ${OSVERSION}로 복구합니다"
-sudo sed -e "s/$FAKEVERSION/$OSVERSION/" -i '' /System/Library/CoreServices/SystemVersion.plist
-SYSTEM_VERSION=$(grep $OSVERSION /System/Library/CoreServices/SystemVersion.plist | cut -d "<" -f 2 | cut -d ">" -f 2)
-echo "시스템 버전 확인: $SYSTEM_VERSION"
-echo
+echo "Nvidia ${PKG_FILE} 설치 스크립트 입니다."
+sudo echo ""
+if [ "${PKG_OSVERSION}" != "${OSVERSION}" ]; then
+        echo "시스템 빌드번호 변경: ${OSVERSION} -> ${PKG_OSVERSION}"
+        sudo sed -e "s/$OSVERSION/$PKG_OSVERSION/" -i '' ${SYSTEM_VERSION_FILE}
+        SYSTEM_OSVERSION=$(grep $PKG_OSVERSION ${SYSTEM_VERSION_FILE} | cut -d "<" -f 2 | cut -d ">" -f 2)
+        echo "시스템 빌드번호 확인: $SYSTEM_OSVERSION $(sw_vers -buildVersion)"
+        echo
+fi
 
-BEFORE_NUMBER=$(grep 17 /Library/Extensions/NVDAStartupWeb.kext/Contents/Info.plist | cut -d "<" -f 2 | cut -d ">" -f 2)
-sudo sed -e 's/>17.*</>17</' -i '' /Library/Extensions/NVDAStartupWeb.kext/Contents/Info.plist
-sudo chown -R root:wheel /Library/Extensions/NVDAStartupWeb.kext
-AFTER_NUMBER=$(grep 17 /Library/Extensions/NVDAStartupWeb.kext/Contents/Info.plist | cut -d "<" -f 2 | cut -d ">" -f 2)
-echo "NVDAStartupWeb.kext: 변경전(${BEFORE_NUMBER}), 변경후(${AFTER_NUMBER})"
-echo                               
-                                   
-echo "kextcahe 재생성: "
-sudo kextcache -Boot -i /
+if [ ! -f $PKG_FILE ]; then
+        echo "패키지 다운로드: ${PKG_URL}"
+        curl ${PKG_URL} --output ${PKG_FILE}
+        echo
+fi
+
+if [ -f $PKG_FILE ]; then
+        echo "패키지 설치: sudo installer -pkg $PKG_FILE -target /"
+        echo
+        sudo installer -pkg $PKG_FILE -target /
+        echo
+fi
+
+if [ "${PKG_OSVERSION}" != "${OSVERSION}" ]; then
+        echo "시스템 빌드번호 복구: ${PKG_OSVERSION} -> ${OSVERSION}"
+        sudo sed -e "s/$PKG_OSVERSION/$OSVERSION/" -i '' ${SYSTEM_VERSION_FILE}
+        SYSTEM_OSVERSION=$(grep $OSVERSION ${SYSTEM_VERSION_FILE} | cut -d "<" -f 2 | cut -d ">" -f 2)
+        echo "시스템 빌드번호 확인: $SYSTEM_OSVERSION $(sw_vers -buildVersion)"
+        echo
+fi
+
+NVDASTARTUPWEB_INFO=/Library/Extensions/NVDAStartupWeb.kext/Contents/Info.plist
+if [ -f ${NVDASTARTUPWEB_INFO} ]; then
+        BEFORE_NUMBER=$(grep 17 ${NVDASTARTUPWEB_INFO} | cut -d "<" -f 2 | cut -d ">" -f 2)
+        sudo sed -e 's/>17.*</>17</' -i '' ${NVDASTARTUPWEB_INFO}
+        sudo chown -R root:wheel ${NVDASTARTUPWEB_INFO}
+        AFTER_NUMBER=$(grep 17 ${NVDASTARTUPWEB_INFO} | cut -d "<" -f 2 | cut -d ">" -f 2)
+        echo "NVDAStartupWeb.kext 변경: ${BEFORE_NUMBER} -> ${AFTER_NUMBER}"
+        echo                               
+                                        
+        echo "kextcahe 재생성: sudo kextcache -Boot -i /"
+        sudo kextcache -Boot -i /
+fi
 
 if [ "$WEBDRIVER_PKG" == "pkg" ]; then
         read -p "시스템을 재시작 하겠습니까? (y/N): " choice
