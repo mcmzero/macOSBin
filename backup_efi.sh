0d4e592d14497ce451db72019dafa497abc36dba
changmin <changmin811@gmail.com>
2017-11-03 09:53:35 +0900

    new commit

--- backup_efi.sh
+++ backup_efi.sh
@@ -2,7 +2,12 @@
 
 DMD=$(date -u +%y%m%d)
 if [ "$(mount | grep ESP)" != "" ]; then
-	tar cvzf ~/Documents/Clover/efi_$DMD.tgz -C /Volumes/ESP .
+	TARGET_DIR="/Volumes/ESP"
 elif [ "$(mount | grep EFI)" != "" ]; then
-	tar cvzf ~/Documents/Clover/efi_$DMD.tgz -C /Volumes/EFI .
+	TARGET_DIR="/Volumes/EFI"
 fi
+
+EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems"
+
+#tar cvzf ~/Documents/Clover/efi_$DMD.tgz --exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd -C $TARGET_DIR .
+tar cvzf ~/Documents/Clover/efi_$DMD.tgz ${EXCLUDE_TAR} -C $TARGET_DIR .
--- backup_wow.sh
+++ backup_wow.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems"
 WOW_PATH=~/Games/World\ of\ Warcraft
-tar cvzf "${WOW_PATH}/WTF/AccountBackup/Account.tgz" -C "${WOW_PATH}/WTF" Account
-tar cvzf "${WOW_PATH}/Interface/AddOnsBackup/AddOns.tgz" -C "${WOW_PATH}/Interface" AddOns
+tar cvzf "${WOW_PATH}/WTF/AccountBackup/Account.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/WTF" Account
+tar cvzf "${WOW_PATH}/Interface/AddOnsBackup/AddOns.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/Interface" AddOns
