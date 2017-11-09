285d9b50d9bb797bce2c43055c9b63ef3dd4c06c
Changmin <changmin811@gmail.com>
2017-11-09 17:40:23 +0900

    경로 변경

--- backup_wow.sh
+++ backup_wow.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems"
-#WOW_PATH=/Volumes/Applications/World\ of\ Warcraft
-WOW_PATH=/Users/changmin/wow
-tar cvzf "${WOW_PATH}/WTF/AccountBackup/Account.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/WTF" Account
-tar cvzf "${WOW_PATH}/Interface/AddOnsBackup/AddOns.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/Interface" AddOns
+WOW_PATH="/Volumes/Archives/Applications/Games/World of Warcraft"
+BAK_PATH="/Volumes/Archives/Google Drive/게임/wow"
+tar cvzf "${BAK_PATH}/AccountBackup/Account.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/WTF" Account
+tar cvzf "${BAK_PATH}/AddOnsBackup/AddOns.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/Interface" AddOns
