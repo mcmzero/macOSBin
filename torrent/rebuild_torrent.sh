86499603d83a0676f2d692c9b752a457043322d4
Changmin <changmin811@gmail.com>
2017-11-09 20:52:35 +0900

    Cleanup

--- torrent/rebuild_torrent.sh
+++ torrent/rebuild_torrent.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
+RAS_SOURCE_PATH=/media/torrent
+RAS_TARGET_PATH=/media/torrent/동영상
+
+TORRENT_SOURCE_PATH=/Share/nas
+TORRENT_TARGET_PATH=/Share/nas/동영상
+
+MCM_SOURCE_PATH=/Volumes/Movies
+MCM_TARGET_PATH=/Volumes/Movies
+
 [ "$(hostname -s |cut -c 1-4)" == "iMac" ] && SAY_MODE=ON
 
 function cleanup() {
-	SRC_DIR=$1
+	SRC_PATH=$1
 	if [ "${2}" != "" ]; then
-		TRG_DIR=$2
+		TAR_PATH=$2
 	else
-		TRG_DIR=$1
+		TAR_PATH=$1
 	fi
 
-	cd ${TRG_DIR}
-	echo "[Cleanup ${TRG_DIR}]"
+	cd ${TAR_PATH}
+	echo "[Cleanup ${TAR_PATH}]"
 	[ "${SAY_MODE}" == "ON" ] && say "정리"
 
-	for FILE in *; do
-		if [ "$(echo ${FILE} | cut -c 1)" == "[" ]; then
-			mv "${FILE}" ".${FILE}" 2> /dev/null
-		fi
-	done
-
-	find "${TRG_DIR}" \( -name "*.mp4" -or -name "*.mkv" -or -name "*.avi" -or -name "*.smi" -or -name "*.sup" \) -print0 | xargs -0 -I % mv % ${SRC_DIR} 2> /dev/null
+	if [ "$(uname)" == "Darwin" ]; then
+		find "${TAR_PATH}" \( -name "*.mp4" -or -name "*.mkv" -or -name "*.avi" -or -name "*.smi" -or -name "*.sup" \) -print0 | xargs -0 -J % mv % ${SRC_PATH}
+	else
+		find "${TAR_PATH}" \( -name "*.mp4" -or -name "*.mkv" -or -name "*.avi" -or -name "*.smi" -or -name "*.sup" \) -print0 | xargs -0 -I % mv % ${SRC_PATH}
+	fi
 
 	for FILE in *; do
 		if [ -d "${FILE}" ] && [ "$(echo ${FILE} | cut -c 1)" != "[" ]; then
@@ -29,11 +36,6 @@ function cleanup() {
 		fi
 	done
 
-	for FILE in .*; do
-        if [ "$(echo ${FILE} | cut -c 1-2)" == ".[" ]; then
-			mv "${FILE}" "$(echo $FILE | sed -e 's/^.\[/\[/')" 2> /dev/null
-        fi
-	done
 	echo
 }
 
@@ -50,40 +52,31 @@ function get_target_name() {
 	echo -n "${GET_TARGET_NAME}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
 }
 
-function get_target_dir_name() {
+function get_target_PATH_name() {
 	echo $@ | cut -d . -f 1 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
 }
 
 function rebuild_mp4_catagory() {
-	SRC_DIR=$1
+	SRC_PATH=$1
 	if [ "${2}" != "" ]; then
-		TRG_DIR=$2
+		TAR_PATH=$2
 	else
-		TRG_DIR=$1
+		TAR_PATH=$1
 	fi
 
-	if [ "${SRC_DIR}" != "${TRG_DIR}" ]; then
-		echo "[Prepair ${SRC_DIR}]"
-		[ "${SAY_MODE}" == "ON" ] && say "준비"
-		find "${SRC_DIR}" \( -name "*.mp4" -or -name "*.mkv" -or -name "*.avi" -or -name "*.smi" -or -name "*.sup" \) -print0 | xargs -0 -I % mv % "${TRG_DIR}" 2> /dev/null
-		echo "[${TRG_DIR}/*.{mp4,mkv,avi,smi,sup}] -> [${SRC_DIR}]"
-		mv ${TRG_DIR}/*.{mp4,mkv,avi,smi,sup} ${SRC_DIR} 2> /dev/null
-		echo
-	fi
-
-	cd ${SRC_DIR}
-	echo "[Rebuild ${SRC_DIR}]"
+	cd ${SRC_PATH}
+	echo "[Rebuild ${SRC_PATH}]"
 	[ "${SAY_MODE}" == "ON" ] && say "재생성"
 
 	for FILE in *.{mp4,mkv,avi,smi,sup}; do
 		if [ "${FILE}" != "*.mp4" ] && [ "${FILE}" != "*.mkv" ] && [ "${FILE}" != "*.avi" ] && [ "${FILE}" != "*.smi" ] && [ "${FILE}" != "*.sup" ]; then
 			TARGET_NAME=$(get_target_name "$FILE")
-			DIR_NAME=${TRG_DIR}/$(get_target_dir_name "$TARGET_NAME")
+			DIR_NAME=${TAR_PATH}/$(get_target_PATH_name "$TARGET_NAME")
 			if [ ! -d "${DIR_NAME}" ]; then
 				echo "+[${DIR_NAME}]"
 				mkdir -p "${DIR_NAME}"
 			fi
-			mv "${SRC_DIR}/${FILE}" "${DIR_NAME}/${TARGET_NAME}" 2> /dev/null
+			mv "${SRC_PATH}/${FILE}" "${DIR_NAME}/${TARGET_NAME}" 2> /dev/null
 		fi
 	done
 	echo
@@ -91,37 +84,33 @@ function rebuild_mp4_catagory() {
 
 function rebuild_raspberryPi() {
 	echo "[Rebuild `hostname -s`]"
-	SOURCE_DIR=/media/torrent
 	case "${1}" in
-	cleanup)
-		cleanup ${SOURCE_DIR}
-		rebuild_mp4_catagory ${SOURCE_DIR}
+	c*)
+		cleanup ${RAS_SOURCE_PATH} ${RAS_TARGET_PATH}
+		rebuild_mp4_catagory ${RAS_SOURCE_PATH} ${RAS_TARGET_PATH}
 	;;
 	*)
-		rebuild_mp4_catagory ${SOURCE_DIR}
+		rebuild_mp4_catagory ${RAS_SOURCE_PATH} ${RAS_TARGET_PATH}
 	;;
 	esac
 }
 
 function rebuild_changmin() {
 	echo "[Rebuild `hostname -s`]"
-	TORRENT_DIR=/Share/nas
-	SOURCE_DIR=/Volumes/Movies
-	TARGET_DIR=/Volumes/Movies/TV
 	case "${1}" in
 	r1c*)
-			cleanup ${TORRENT_DIR}
-			rebuild_mp4_catagory ${TORRENT_DIR}
+			cleanup ${TORRENT_SOURCE_PATH} ${TORRENT_TARGET_PATH}
+			rebuild_mp4_catagory ${TORRENT_SOURCE_PATH} ${TORRENT_TARGET_PATH}
 	;;
 	r1)
-			rebuild_mp4_catagory ${TORRENT_DIR}
+			rebuild_mp4_catagory ${TORRENT_SOURCE_PATH} ${TORRENT_TARGET_PATH}
 	;;
 	c*)
-			cleanup ${SOURCE_DIR} ${TARGET_DIR}
-			rebuild_mp4_catagory ${SOURCE_DIR} ${TARGET_DIR}
+			cleanup ${MCM_SOURCE_PATH} ${MCM_TARGET_PATH}
+			rebuild_mp4_catagory ${MCM_SOURCE_PATH} ${MCM_TARGET_PATH}
 	;;
 	*)
-			rebuild_mp4_catagory ${SOURCE_DIR} ${TARGET_DIR}
+			rebuild_mp4_catagory ${MCM_SOURCE_PATH} ${MCM_TARGET_PATH}
 	;;
 	esac
 }
