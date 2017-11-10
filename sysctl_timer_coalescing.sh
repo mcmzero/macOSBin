89e32a9e6ad1d7362e1b4518190f01efef88df50
Changmin <changmin811@gmail.com>
2017-11-10 21:20:48 +0900

    Coalescing on off 기능 추가

---
+++ sysctl_timer_coalescing.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SYSCTL_PLIST=/Library/LaunchDaemons/sysctl.plist
+
+if [ "$1" == "on" ]; then
+	sudo sed -e 's/=0/=1/' -i '' $SYSCTL_PLIST
+	sudo launchctl unload $SYSCTL_PLIST
+	sudo launchctl load $SYSCTL_PLIST
+	sudo sysctl kern.timer.coalescing_enabled
+elif [ "$1" == "off" ]; then
+        sudo sed -e 's/=1/=0/' -i '' $SYSCTL_PLIST
+        sudo launchctl unload $SYSCTL_PLIST
+        sudo launchctl load $SYSCTL_PLIST
+        sudo sysctl kern.timer.coalescing_enabled
+else
+	echo "sysctl_timer_coalescing.sh {on|off}"
+	sudo sysctl kern.timer.coalescing_enabled
+fi
--- sysctl_timer_coalescing_disable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo launchctl load /Library/LaunchDaemons/sysctl.plist
-sudo sysctl -w kern.timer.coalescing_enabled=0
-echo
-echo sudo vi /Library/LaunchDaemons/sysctl.plist
