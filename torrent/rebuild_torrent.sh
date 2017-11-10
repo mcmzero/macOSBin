a42dcae702317d65016159c15d6cc0896fd86c4a
changmin <changmin811@gmail.com>
2017-11-10 17:20:13 +0900

    패턴 변경

--- torrent/rebuild_torrent.sh
+++ torrent/rebuild_torrent.sh
@@ -44,11 +44,12 @@ function get_target_name() {
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/....[pP]-[nN][eE][xX][tT]//' -e 's/....[pP]-[wW][iI][tT][hH]//' -e 's/....[pP]-[cC][iI][nN][eE][bB][uU][sS]//')
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\.[aA][aA][cC]//' -e 's/\.[hH][dD][tT][vV]//' -e 's/\.[hH]26[45]//' -e 's/\.[eE][nN][dD]//')
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\.[hH][eE][vV][cC]//' -e 's/\.10[bB][iI][tT]//' -e 's/\.[xX]26[45]//' -e 's/\.[bB][lL][uU][rR][aA][yY]//')
-	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\.[wW][eE][bB]-[dD][lL]//' -e 's/-.*\././' -e 's/\.5\.1//' -e 's/\.[xX]26[45]//' -e 's/\.[bB][lL][uU][rR][aA][yY]//')
+	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\.[wW][eE][bB]-[dD][lL]//' -e 's/\.5\.1//' -e 's/\.[xX]26[45]//' -e 's/\.[bB][lL][uU][rR][aA][yY]//')
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\.\.\./\./' -e 's/\.\./\./' -e 's/알\.쓸\.신\./알쓸신/' -e 's/AMZN//' -e 's/Game.of.Thrones/Game of Thrones/')
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/ .부\././' -e 's/\.[wW][eE][bB][rR][iI][pP]//')
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\.1440[pP]//' -e 's/\.1080[pP]//' -e 's/\.720[pP]//' -e 's/\.360[pP]//')
 	GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/\(^[0-9]*\)\.\([^\.]*\.\)/\2\1./')
+	#GET_TARGET_NAME=$(echo -n "${GET_TARGET_NAME}" | sed -e 's/-.*\././')
 	echo -n "${GET_TARGET_NAME}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
 }
 
