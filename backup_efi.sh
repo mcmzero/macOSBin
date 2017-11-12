92ae91a872b703fc4e0c4bab2547536283578a5b
Changmin <changmin811@gmail.com>
2017-11-12 15:45:06 +0900

    경로 수정 및 항목 추가

--- backup_efi.sh
+++ backup_efi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TARGET_PATH="/Volumes/Archives/Google Drive/컴퓨터/Clover"
+TARGET_PATH="$HOME/drv/컴퓨터/Clover"
 
 DMD_UTC=$(date -u +%y%m%d-%H)
 DMD=$(date +%y%m%d-%H)
@@ -12,4 +12,9 @@ fi
 
 EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems --exclude .svn"
 
+EFI_DEV=disk0s1
+diskutil umount $EFI_DEV
+sudo dd if=/dev/$EFI_DEV bs=64k | gzip -c > "${TARGET_PATH}"/efi_${DMD}.img.gz
+diskutil mount $EFI_DEV
 tar cvzf "${TARGET_PATH}"/efi_${DMD}.tgz ${EXCLUDE_TAR} -C ${SOURCE_DIR} .
+ls -lh "${TARGET_PATH}"
--- backup_wow.sh
+++ backup_wow.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+WOW_PATH="/Applications/Games/World of Warcraft"
+BAK_PATH="$HOME/drv/게임/wow"
 
 EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems"
-WOW_PATH="/Volumes/Archives/Applications/Games/World of Warcraft"
-BAK_PATH="/Volumes/Archives/Google Drive/게임/wow"
 tar cvzf "${BAK_PATH}/AccountBackup/Account.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/WTF" Account
 tar cvzf "${BAK_PATH}/AddOnsBackup/AddOns.tgz" ${EXCLUDE_TAR} -C "${WOW_PATH}/Interface" AddOns
