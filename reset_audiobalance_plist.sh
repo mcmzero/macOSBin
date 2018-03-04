ee2e0766b69aa6435ebf3ec2f0634e38d5e7721c
mcmZero <changmin811@gmail.com>
2018-03-04 21:00:51 +0900

    volume setting script

--- reset_audiobalance_plist.sh
+++ reset_audiobalance_plist.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
-/usr/bin/osascript -e "set volume output volume (output volume of (get volume settings))"
-
 PATH_PLIST=~/Library/LaunchAgents/com.mcm.resetaudiobalance.plist
 launchctl unload $PATH_PLIST
 launchctl load $PATH_PLIST
+/usr/bin/osascript -e "set volume input volume (1)" -e "set volume output volume (output volume of (get volume settings))"
