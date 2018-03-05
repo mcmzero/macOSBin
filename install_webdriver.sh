6a6de4793808505f66f3125d9af707bfdc0df408
mcmZero <changmin811@gmail.com>
2018-03-06 05:56:07 +0900

    bug fix

--- install_webdriver.sh
+++ install_webdriver.sh
@@ -3,10 +3,8 @@
 # 2017.11.6 <changmin811@gmail.com>
 
 PKG_MACOS_17=( \
-        "387.10.10.10.25.159 17E160g" \
-        "387.10.10.10.25.158 17E160e" \
-        "387.10.10.10.25.159 17D2102" \
-        "387.10.10.10.25.158 17D102" \
+        "387.10.10.10.25.159 17E160g 17D2102" \
+        "387.10.10.10.25.158 17E160e 17D102" \
         "378.10.10.10.25.106 17C2205" \
 )
 PKG_DEFAULT_17=$((${#PKG_MACOS_17[@]} - 1))
@@ -36,6 +34,7 @@ fi
 unset -v PKG_MACOS_16 PKG_MACOS_17
 unset PKG_DEFAULT_16 PKG_DEFAULT_17
 
+#help
 if [ "$1" == "-h" ]; then
         echo [PKG Version] [OS Version]
         echo [Install commands]
@@ -47,32 +46,37 @@ if [ "$1" == "-h" ]; then
         done
         exit
 fi
+
 PKG_FILE_VERSION=$(echo ${PKG_MACOS[PKG_DEFAULT]} | cut -d ' ' -f 1)
 PKG_OSVERSION=$(echo ${PKG_MACOS[PKG_DEFAULT]} | cut -d ' ' -f 2)
 for NUM in ${!PKG_MACOS[@]}; do
+        W=($(echo ${PKG_MACOS[NUM]}|wc -w))
         if [ "$1" != "" ]; then
                 if [ "$1" == "$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 1 | cut -d '.' -f 6)" ]; then
                         PKG_FILE_VERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 1)
-                        PKG_OSVERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 2)
+                        PKG_OSVERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f $W)
                         break
                 fi
         else
-                if [ "$OSVERSION" == "$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 2)" ]; then
-                        PKG_FILE_VERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 1)
-                        PKG_OSVERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 2)
-                        break
-                fi
+                for FNUM in $(eval echo {2..$W}); do
+                        if [ "$OSVERSION" == "$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f $FNUM)" ]; then
+                                PKG_FILE_VERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f 1)
+                                PKG_OSVERSION=$(echo ${PKG_MACOS[NUM]} | cut -d ' ' -f $W)
+                                break
+                        fi
+                done
         fi
 done
 echo "macOS ($OSVERSION) : $PKG_FILE_VERSION ($PKG_OSVERSION)"
 
+cd $HOME/Downloads
 PKG_FILE=WebDriver-${PKG_FILE_VERSION}.pkg
 PKG_MAJOR_VERSION=$(echo $PKG_FILE_VERSION | cut -d . -f 1)
 PKG_URL=https://images.nvidia.com/mac/pkg/${PKG_MAJOR_VERSION}/${PKG_FILE}
 
 echo "Download & Install Nvidia $PKG_FILE"
 sudo echo ""
-if [ ! -f "$PKG_FILE" ]; then
+if ! pkgutil --check-signature $PKG_FILE &> /dev/null; then
         echo "Download: $PKG_URL"
         curl $PKG_URL --output $PKG_FILE || exit
         echo
@@ -91,27 +95,25 @@ if [ "$OSVERSION" == "" ]; then
         OSVERSION=$(sw_vers -buildVersion)
 fi
 
-if [ "$PKG_OSVERSION" != "$OSVERSION" ]; then
-        echo "Change system build version: $OSVERSION -> $PKG_OSVERSION"
-        set_ProductBuildVersion "$PKG_OSVERSION"
-        SYSTEM_OSVERSION=$(print_ProductBuildVersion)
-        echo "Check system build version: $SYSTEM_OSVERSION $(sw_vers -buildVersion)"
-        echo
-fi
-
+# install pkg
 if [ -f "$PKG_FILE" ]; then
-        echo "Install package: sudo installer -pkg $PKG_FILE -target /"
-        echo
+        if [ "$PKG_OSVERSION" != "$OSVERSION" ]; then
+                echo "Change system build version: $OSVERSION -> $PKG_OSVERSION"
+                set_ProductBuildVersion "$PKG_OSVERSION"
+                SYSTEM_OSVERSION=$(print_ProductBuildVersion)
+                echo "Check system build version: $SYSTEM_OSVERSION $(sw_vers -buildVersion)"
+                echo
+        fi
+        #echo "Install package: sudo installer -pkg $PKG_FILE -target /"
         sudo installer -pkg $PKG_FILE -target /
         echo
-fi
-
-if [ "$PKG_OSVERSION" != "$OSVERSION" ]; then
-        echo "Recover system build version: $PKG_OSVERSION -> $OSVERSION"
-        set_ProductBuildVersion "$OSVERSION"
-        SYSTEM_OSVERSION=$(print_ProductBuildVersion)
-        echo "Check system build version: $SYSTEM_OSVERSION $(sw_vers -buildVersion)"
-        echo
+        if [ "$PKG_OSVERSION" != "$OSVERSION" ]; then
+                echo "Recover system build version: $PKG_OSVERSION -> $OSVERSION"
+                set_ProductBuildVersion "$OSVERSION"
+                SYSTEM_OSVERSION=$(print_ProductBuildVersion)
+                echo "Check system build version: $SYSTEM_OSVERSION $(sw_vers -buildVersion)"
+                echo
+        fi
 fi
 
 function print_NVDARequiredOS() {
@@ -130,5 +132,5 @@ if [ -f "$NVDASTARTUPWEB_INFO" ]; then
         AFTER_NUMBER=$(print_NVDARequiredOS)
         echo "Patch NVDAStartupWeb.kext:NVDARequiredOS: $BEFORE_NUMBER -> $AFTER_NUMBER" && echo                               
         echo "Rebuild kextcahe: sudo kextcache -Boot -i /" && sudo kextcache -Boot -i / && echo
-        rm "$PKG_FILE"
+        #rm "$PKG_FILE"
 fi
