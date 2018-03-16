fd7ca8e57cd9f97b9665c162d702e362fd78e513
mcmZero <changmin811@gmail.com>
2018-03-16 12:27:01 +0900

    efi 마운트 추

--- backup_efi.sh
+++ backup_efi.sh
@@ -1,21 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-EFI_DEV=rdisk0s1
-TARGET_PATH="$HOME/drv/Computer.localized/Clover"
-EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems --exclude .svn"
-
-DMD_UTC=$(date -u +%y%m%d-%H)
-DMD=$(date +%y%m%d-%H)
-if [ "$(mount | grep ESP)" != "" ]; then
-	SOURCE_DIR="/Volumes/ESP"
-elif [ "$(mount | grep EFI)" != "" ]; then
-	SOURCE_DIR="/Volumes/EFI"
+EFI_DEV=($(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2))
+EFI_MOUNT=$(mount|grep ${EFI_DEV}|cut -d ' ' -f 3)
+[ "$EFI_MOUNT" == "" ] && diskutil mount $EFI_DEV
+[ "$EFI_MOUNT" != "" ] && echo $EFI_MOUNT
+if [ "$EFI_MOUNT" == "" ]; then
+	echo "Cannot find EFI directory"
+	exit 1
 fi
 
+SOURCE_DIR=$EFI_MOUNT
+TARGET_PATH="$HOME/drv/Computer.localized/Clover"
+EXCLUDE_TAR="--exclude .Spotlight-V100 --exclude .Trashes --exclude .fseventsd --exclude .TemporaryItems --exclude .svn"
+DMD_UTC=$(date -u +%y%m%d-%H)
+DMD=$(date +%y%m%d-%H)
+
 #sudo tar czvf "${TARGET_PATH}"/efi_${DMD}.tgz ${EXCLUDE_TAR} -C ${SOURCE_DIR} .
-#sudo dd if=/dev/$EFI_DEV bs=1m | gzip -c > "${TARGET_PATH}"/efi_${DMD}_dd.img.gz
+#sudo dd if=/dev/r${EFI_DEV} bs=1m | gzip -c > "${TARGET_PATH}"/efi_${DMD}_dd.img.gz
 
 sudo tar czvf "${TARGET_PATH}"/efi_${DMD}.tgz -C ${SOURCE_DIR} .
-sudo hdiutil create -ov "${TARGET_PATH}"/efi_${DMD}.dmg -srcdevice /dev/$EFI_DEV
+sudo hdiutil create -ov "${TARGET_PATH}"/efi_${DMD}.dmg -srcdevice /dev/r$EFI_DEV
 sudo chown changmin "${TARGET_PATH}"/*
+
+rsync -auzv --delete ${TARGET_PATH}/ $HOME/Seafile/EFI
+
 ls -lh "${TARGET_PATH}"
--- efi_mount.sh
+++ efi_mount.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+mount_efi() {
 diskutil mount $(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2)
+}
+
+EFI_DEV=($(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2))
+EFI_MOUNT=$(mount|grep ${EFI_DEV}|cut -d ' ' -f 3)
+[ "$EFI_MOUNT" == "" ] && diskutil mount $EFI_DEV
+[ "$EFI_MOUNT" != "" ] && echo $EFI_MOUNT
