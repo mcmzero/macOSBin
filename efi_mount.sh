ff39f853094374471cd8f8bbbf206aa7bd08f20d
mcmZero <changmin811@gmail.com>
2018-03-20 13:47:01 +0900

    마운트 후 파인더 열

--- efi_mount.sh
+++ efi_mount.sh
@@ -6,5 +6,9 @@ diskutil mount $(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2)
 
 EFI_DEV=($(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2))
 EFI_MOUNT=$(mount|grep ${EFI_DEV}|cut -d ' ' -f 3)
-[ "$EFI_MOUNT" == "" ] && diskutil mount $EFI_DEV
-[ "$EFI_MOUNT" != "" ] && echo $EFI_MOUNT
+if [ "$EFI_MOUNT" == "" ]; then
+	diskutil mount $EFI_DEV
+	EFI_MOUNT=$(mount|grep ${EFI_DEV}|cut -d ' ' -f 3)
+fi
+echo $EFI_MOUNT
+open $EFI_MOUNT
