68bc37c615ce82d4d2af6766a3e823f6ad0a901e
mcmZero <changmin811@gmail.com>
2018-03-27 23:32:50 +0900

    스크립트 정리

--- apfs_efi.sh
+++ apfs_efi.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-[ -d /Volumes/ESP/EFI/CLOVER/drivers64UEFI ] && rsync -av /usr/standalone/i386/apfs.efi /Volumes/ESP/EFI/CLOVER/drivers64UEFI/apfs.efi
-[ -d /Volumes/ESP/EFI/CLOVER/drivers64UEFI ] && sudo perl -i -pe 's|\x00\x74\x07\xb8\xff\xff|\x00\x90\x90\xb8\xff\xff|sg' /Volumes/ESP/EFI/CLOVER/drivers64UEFI/apfs.efi
-
-[ -d /Volumes/EFI/EFI/CLOVER/drivers64UEFI ] && rsync -av /usr/standalone/i386/apfs.efi /Volumes/EFI/EFI/CLOVER/drivers64UEFI/apfs.efi
-[ -d /Volumes/EFI/EFI/CLOVER/drivers64UEFI ] && sudo perl -i -pe 's|\x00\x74\x07\xb8\xff\xff|\x00\x90\x90\xb8\xff\xff|sg' /Volumes/EFI/EFI/CLOVER/drivers64UEFI/apfs.efi
+efiDev=($(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2))
+efiMount=$(mount|grep ${efiDev}|cut -d ' ' -f 3)
+if [ "$efiMount" == "" ]; then
+	diskutil mount $efiDev
+	efiMount=$(mount|grep ${efiDev}|cut -d ' ' -f 3)
+fi
+pathDrivers64UEFI=${efiMount}/EFI/CLOVER/drivers64UEFI
+if [ -d ${pathDrivers64UEFI} ]; then
+	rsync -av /usr/standalone/i386/apfs.efi $pathDrivers64UEFI
+	#sudo perl -i -pe 's|\x00\x74\x07\xb8\xff\xff|\x00\x90\x90\xb8\xff\xff|sg' ${pathDrivers64UEFI}/apfs.efi
+fi
--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-diskutil mount $(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2)
+efiDev=($(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2))
+efiMount=$(mount|grep ${efiDev}|cut -d ' ' -f 3)
+if [ "$efiMount" == "" ]; then
+	diskutil mount $efiDev
+	efiMount=$(mount|grep ${efiDev}|cut -d ' ' -f 3)
+fi
+
+pathDrivers64UEFI=${efiMount}/EFI/CLOVER/drivers64UEFI
+if [ -d ${pathDrivers64UEFI} ]; then
+	rsync -av /usr/standalone/i386/apfs.efi $pathDrivers64UEFI
+	#sudo perl -i -pe 's|\x00\x74\x07\xb8\xff\xff|\x00\x90\x90\xb8\xff\xff|sg' ${pathDrivers64UEFI}/apfs.efi
+fi
+
 audio_cloverALC-130_v0.3.command << __EOF__
 y
 y
 y
 __EOF__
-apfs_efi.sh
 install_webdriver.sh -u
+
