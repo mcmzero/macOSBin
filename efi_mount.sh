a07668de2e9993b018f742972ee426bf19ae664c
mcmZero <changmin811@gmail.com>
2018-04-07 09:05:08 +0900

    비교 코드 수정

--- efi_mount.sh
+++ efi_mount.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 mount_efi() {
-diskutil mount $(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2)
+	diskutil mount $(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2)
 }
 
 EFI_DEV=($(diskutil list|grep EFI|head -n 1|cut -d 'B' -f 2))
 EFI_MOUNT=$(mount|grep ${EFI_DEV}|cut -d ' ' -f 3)
-if [ "$EFI_MOUNT" == "" ]; then
+if [ -z "$EFI_MOUNT" ]; then
 	diskutil mount $EFI_DEV
 	EFI_MOUNT=$(mount|grep ${EFI_DEV}|cut -d ' ' -f 3)
 fi
 echo $EFI_MOUNT
-open $EFI_MOUNT
+#open $EFI_MOUNT
