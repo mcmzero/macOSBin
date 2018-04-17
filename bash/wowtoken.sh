2a85aa03866520675c884324d732228ff7cb7a8b
mcmZero <changmin811@gmail.com>
2018-04-18 06:36:41 +0900

    watch 반복 옵션 추가

--- bash/wowtoken.sh
+++ bash/wowtoken.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-function current_price() {
-	jq .korea.current_price <(curl -s http://wowtokenprices.com/current_prices.json)
-}
-current_price
+[[ $1 ]] && watch -n $1 "curl -s http://wowtokenprices.com/current_prices.json|jq .korea.current_price"\
+            || jq .korea.current_price <(curl -s http://wowtokenprices.com/current_prices.json)
