f4646d6b62e943eca0492e723339b7363cc94216
mcmZero <changmin811@gmail.com>
2018-04-21 13:40:12 +0900

    출력 메세지 변경

--- bash/afreecatv.sh
+++ bash/afreecatv.sh
@@ -3,8 +3,8 @@
 if [[ $(ps -ef| grep MACafpackage | wc | awk '{print $1}') -ge 2 ]]; then
 	killall -9 MACafpackage
 	[[ $(ps -ef| grep MACafstreamer | wc | awk '{print $1}') -ge 2 ]] && killall -9 MACafstreamer
-    echo turn off afrecca agent
+    echo Turn off AfreccaTV agent
 else
     open -a "MACafpackage"
-    echo turn on afrecca agent
+    echo Turn on AfreccaTV agent
 fi
