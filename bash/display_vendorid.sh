8405307e7fc4d5cc66b6b89e72f8e5fa13efaf97
mcmZero <changmin811@gmail.com>
2018-04-28 15:43:30 +0900

    스크립트 정리

--- bash/display_vendorid.sh
+++ bash/display_vendorid.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-rm -rf /System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-e4f
-cp -afv /Volumes/Backup.localized/Archives.localized/macOS/DisplayVendorID-e4f /System/Library/Displays/Contents/Resources/Overrides/
+case $1 in
+    rm)
+        sudo rm -rf /System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-e4f
+    ;;
+    cp)
+        sudo cp -afv /Volumes/Backup.localized/Archives.localized/macOS/DisplayVendorID-e4f /System/Library/Displays/Contents/Resources/Overrides/
+    ;;
+    ls)
+        ls -l /System/Library/Displays/Contents/Resources/Overrides
+    ;;
+esac
